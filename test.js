08b369355e4532416b9d6b8c6b645715583d2bf5
NastyaLo <loyko2802@gmail.com>
2018-10-28 20:53:11 +0300

    test: add test to task 'recursion'

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const sumOfOther = require('./sumOfOther');
 const make = require('./make');
+const recursion = require('./recursion');
 
 const sum = function sum(a, b) {
   return a + b;
@@ -26,17 +27,17 @@ describe('Check sum of other elements task', () => {
 
   it('2', () => {
     const sumOfOtherElems = sumOfOther([5, 1, 0, 13, -1]);
-    assert.equal(sumOfOtherElems.toString(), [13, 17, 18, 5, 19].toString());
+    assert.equal(JSON.stringify(sumOfOtherElems), JSON.stringify([13, 17, 18, 5, 19]));
   });
 
   it('3', () => {
     const sumOfOtherElems = sumOfOther([1, 1, 1, 1, 1, 1]);
-    assert.equal(sumOfOtherElems.toString(), [5, 5, 5, 5, 5, 5].toString());
+    assert.equal(JSON.stringify(sumOfOtherElems), JSON.stringify([5, 5, 5, 5, 5, 5]));
   });
 
   it('4', () => {
     const sumOfOtherElems = sumOfOther([0, 0, 0, 0, 0, 0, 0]);
-    assert.equal(sumOfOtherElems.toString(), [0, 0, 0, 0, 0, 0, 0].toString());
+    assert.equal(JSON.stringify(sumOfOtherElems), JSON.stringify([0, 0, 0, 0, 0, 0, 0]));
   });
 });
 
@@ -59,5 +60,13 @@ describe('Check with items storage', () => {
   it('4', () => {
     const makeDivision = make(120)(3)(2)(5)(division);
     assert.equal(makeDivision, 4);
+  });
+});
+
+describe('Check tree creation using recursion', () => {
+  it('1', () => {
+    const myTree = {'value':100,'left':{'value':90,'left':{'value':70},'right':{'value':99}},'right':{'value':120,'left':{'value':110},'right':{'value':130}}};
+    const recursionTree = recursion(myTree);
+    assert.equal(JSON.stringify(recursionTree), JSON.stringify([[100], [90, 120], [70, 99, 110, 130]]));
   });
 });
